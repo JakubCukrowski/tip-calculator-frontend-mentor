67863ecd2f08d68e7ff63ed49bf5d76e9ce22c8d
JakubCukrowski <j.d.cukrowski@gmail.com>
2023-05-27 20:17:46 +0200

    Project finished

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,7 @@
 const moneyAmountInput = document.querySelector("#bill-value");
 const peopleCountInput = document.querySelector("#people-count");
 const tipPrecentButtons = document.querySelectorAll(".tip-percentage-buttons button");
+const customTipInput = document.querySelector("#custom");
 const tipAmountSpan = document.querySelector(".tip-amount");
 const totalValueSpan = document.querySelector(".total-value");
 const errorZero = document.querySelector(".error-zero");
@@ -8,19 +9,24 @@ const errorNegative = document.querySelector(".error-negative");
 const resetBtn = document.querySelector(".reset-btn");
 let percentageValue;
 const handleResult = () => {
+    const valuePerPerson = +moneyAmountInput.value;
+    const peopleCount = +peopleCountInput.value;
     if (peopleCountInput.value !== "0"
         && peopleCountInput.value !== ""
         && +peopleCountInput.value > 0
         && moneyAmountInput.value !== ""
         && +moneyAmountInput.value > 0
+        && +customTipInput.value !== 0
+        && +percentageValue !== 0
         && percentageValue !== undefined) {
-        const result = Number(moneyAmountInput.value) * Number(percentageValue) / Number(peopleCountInput.value);
-        const roundedResult = Math.floor(+result * 100) / 100;
+        const tipResult = +valuePerPerson * +percentageValue / +peopleCount;
+        const roundedResult = Math.floor(+tipResult * 100) / 100;
+        const totalResult = (+tipResult * +peopleCount + +valuePerPerson) / +peopleCount;
+        totalValueSpan.innerText = `$${Number(totalResult.toFixed(2))}`;
         tipAmountSpan.innerText = `$${(roundedResult.toFixed(2))}`;
         resetBtn.removeAttribute("disabled");
-        if (result === Infinity) {
-            tipAmountSpan.innerText = '$0.00';
-            resetBtn.setAttribute("disabled", "");
+        if (tipResult === Infinity) {
+            tipAmountSpan.innerText, totalValueSpan.innerText = '$0.00';
         }
     }
 };
@@ -29,17 +35,19 @@ const handleButtons = (e) => {
     const event = e.target;
     percentageValue = Number(event.dataset.value);
     tipPrecentButtons.forEach(button => button.classList.remove("selected"));
-    if (Number(percentageValue) === Number(event.dataset.value)) {
-        event.classList.add("selected");
-    }
+    event.classList.add("selected");
+    customTipInput.type = "text";
+    customTipInput.value = "Custom";
     handleResult();
 };
 const handlePeopleCountInput = (e) => {
     const event = e.target;
     if (+event.value === 0 || +event.value < 0) {
         tipAmountSpan.innerText = "$0.00";
+        totalValueSpan.innerText = "$0.00";
         event.classList.add("error");
         Number(event.value) === 0 ? errorZero.style.display = "block" : errorNegative.style.display = "block";
+        resetBtn.setAttribute("disabled", "");
     }
     else {
         event.classList.remove("error");
@@ -53,8 +61,51 @@ const handlePeopleCountInput = (e) => {
     }
     handleResult();
 };
+const typeChangeToNumber = (e) => {
+    const event = e.target;
+    if (event.type === "text") {
+        event.type = "number";
+    }
+    tipPrecentButtons.forEach(button => button.classList.remove("selected"));
+    percentageValue = 0;
+    totalValueSpan.innerText = "$0.00";
+    tipAmountSpan.innerText = "$0.00";
+};
+const typeChangeToText = (e) => {
+    const event = e.target;
+    if (event.value === "") {
+        event.type = "text";
+        event.value = "Custom";
+    }
+};
+const handleCustomTip = (e) => {
+    const event = e.target;
+    percentageValue = Number(event.value) * 0.01;
+    handleResult();
+    if (event.value === "" || +event.value === 0) {
+        totalValueSpan.innerText = "$0.00";
+        tipAmountSpan.innerText = "$0.00";
+        resetBtn.setAttribute("disabled", "");
+    }
+};
+const handleReset = (e) => {
+    e.preventDefault();
+    percentageValue = 0;
+    peopleCountInput.value = "";
+    moneyAmountInput.value = "";
+    tipAmountSpan.innerText = '$0.00';
+    totalValueSpan.innerText = '$0.00';
+    tipPrecentButtons.forEach(button => button.classList.remove("selected"));
+    resetBtn.setAttribute("disabled", "");
+    customTipInput.type = "text";
+    customTipInput.value = "Custom";
+};
 tipPrecentButtons.forEach(button => {
     button.addEventListener("click", handleButtons);
 });
 peopleCountInput.addEventListener("input", handlePeopleCountInput);
 moneyAmountInput.addEventListener("input", handleResult);
+resetBtn.addEventListener("click", handleReset);
+customTipInput.addEventListener("click", typeChangeToNumber);
+customTipInput.addEventListener("focusout", typeChangeToText);
+customTipInput.addEventListener("input", handleCustomTip);
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 const moneyAmountInput: HTMLInputElement = document.querySelector("#bill-value")
 const peopleCountInput: HTMLInputElement = document.querySelector("#people-count")
 const tipPrecentButtons: NodeListOf<HTMLButtonElement> = document.querySelectorAll(".tip-percentage-buttons button")
+const customTipInput: HTMLInputElement = document.querySelector("#custom")
 const tipAmountSpan: HTMLSpanElement = document.querySelector(".tip-amount")
 const totalValueSpan: HTMLSpanElement = document.querySelector(".total-value")
 const errorZero: HTMLSpanElement = document.querySelector(".error-zero")
@@ -10,20 +11,26 @@ const resetBtn: HTMLButtonElement = document.querySelector(".reset-btn")
 let percentageValue: Number
 
 const handleResult = () => {
+    const valuePerPerson: Number = +moneyAmountInput.value
+    const peopleCount: Number = +peopleCountInput.value
+
     if (peopleCountInput.value !== "0" 
         && peopleCountInput.value !== ""
         && +peopleCountInput.value > 0 
         && moneyAmountInput.value !== ""
         && +moneyAmountInput.value > 0 
+        && +customTipInput.value !== 0
+        && +percentageValue !== 0
         && percentageValue !== undefined) {
-        const result: Number = Number(moneyAmountInput.value) * Number(percentageValue) / Number(peopleCountInput.value)
-        const roundedResult: Number = Math.floor(+result * 100) / 100
+        const tipResult: Number = +valuePerPerson * +percentageValue / +peopleCount
+        const roundedResult: Number = Math.floor(+tipResult * 100) / 100
+        const totalResult: Number = (+tipResult * +peopleCount + +valuePerPerson) / +peopleCount
+        totalValueSpan.innerText = `$${Number(totalResult.toFixed(2))}`
         tipAmountSpan.innerText = `$${(roundedResult.toFixed(2))}`
         resetBtn.removeAttribute("disabled") 
         
-        if (result === Infinity) {
-            tipAmountSpan.innerText = '$0.00'
-            resetBtn.setAttribute("disabled", "")
+        if (tipResult === Infinity) {
+            tipAmountSpan.innerText, totalValueSpan.innerText = '$0.00'
         }
         
     } 
@@ -34,9 +41,9 @@ const handleButtons = (e: Event) => {
     const event = e.target as HTMLButtonElement
     percentageValue = Number(event.dataset.value)  
     tipPrecentButtons.forEach(button => button.classList.remove("selected"))
-    if (Number(percentageValue) === Number(event.dataset.value)) {
-        event.classList.add("selected")
-    } 
+    event.classList.add("selected")
+    customTipInput.type = "text"
+    customTipInput.value = "Custom"
     handleResult()
 }
 
@@ -44,8 +51,10 @@ const handlePeopleCountInput = (e: Event) => {
     const event = e.target as HTMLInputElement
     if (+event.value === 0 || +event.value < 0) {
         tipAmountSpan.innerText = "$0.00"
+        totalValueSpan.innerText = "$0.00"
         event.classList.add("error")
         Number(event.value) === 0 ? errorZero.style.display = "block" : errorNegative.style.display = "block"
+        resetBtn.setAttribute("disabled", "")
     } else {
         event.classList.remove("error")
         errorZero.style.display = "none"
@@ -61,9 +70,57 @@ const handlePeopleCountInput = (e: Event) => {
     handleResult()
 }
 
+const typeChangeToNumber = (e: Event) => {
+    const event = e.target as HTMLInputElement
+    if (event.type === "text") {
+        event.type = "number"
+    }
+    tipPrecentButtons.forEach(button => button.classList.remove("selected"))
+    percentageValue = 0
+    totalValueSpan.innerText = "$0.00"
+    tipAmountSpan.innerText = "$0.00"
+}
+
+const typeChangeToText = (e: Event) => {
+    const event = e.target as HTMLInputElement
+    if (event.value === "") {
+        event.type = "text"
+        event.value = "Custom"
+    }
+}
+
+const handleCustomTip = (e: Event) => {
+    const event = e.target as HTMLInputElement
+    percentageValue = Number(event.value) * 0.01
+    handleResult()
+
+    if (event.value === "" || +event.value === 0) {
+        totalValueSpan.innerText = "$0.00"
+        tipAmountSpan.innerText = "$0.00"
+        resetBtn.setAttribute("disabled", "")
+    }
+}
+
+const handleReset = (e: Event) => {
+    e.preventDefault()
+    percentageValue = 0
+    peopleCountInput.value = ""
+    moneyAmountInput.value = ""
+    tipAmountSpan.innerText = '$0.00'
+    totalValueSpan.innerText = '$0.00'
+    tipPrecentButtons.forEach(button => button.classList.remove("selected"))
+    resetBtn.setAttribute("disabled", "")
+    customTipInput.type = "text"
+    customTipInput.value = "Custom"
+}
+
 tipPrecentButtons.forEach(button => {
     button.addEventListener("click", handleButtons)
 })
 
 peopleCountInput.addEventListener("input", handlePeopleCountInput)
 moneyAmountInput.addEventListener("input", handleResult)
+resetBtn.addEventListener("click", handleReset)
+customTipInput.addEventListener("click", typeChangeToNumber)
+customTipInput.addEventListener("focusout", typeChangeToText)
+customTipInput.addEventListener("input", handleCustomTip)
